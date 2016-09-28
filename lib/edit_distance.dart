0f98d679acecaf58a3b53566191f03e08cef76c4
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-09-29 03:36:02 +0900

    Add StringDistance interface

--- lib/edit_distance.dart
+++ lib/edit_distance.dart
@@ -1,11 +1,9 @@
 // Copyright (c) 2016, Kwang Yul Seo. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Edit distances algorithms for fuzzy matching
 library edit_distance;
 
-export 'src/edit_distance_base.dart';
+export 'src/base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
