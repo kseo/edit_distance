c3a9dbf4b66040d20a2903ec22044b5890e7cadb
Istvan Soos <istvan.soos@agilord.com>
2016-11-13 09:28:33 +0000

    Add Jaccard N-gram distance

--- README.md
+++ README.md
@@ -12,11 +12,13 @@ Edit distances algorithms for fuzzy matching. Specifically, this library provide
 * [Restricted Damerau-Levenshtein distance][Damerau]
 * [Longest Common Subsequence][LongestCommonSubsequence]
 * [Jaro–Winkler distance][JaroWinkler]
+* [Jaccard N-gram distance][Jaccard]
 
 [Levenshtein]: https://en.wikipedia.org/wiki/Levenshtein_distance
 [Damerau]: https://en.wikipedia.org/wiki/Damerau%E2%80%93Levenshtein_distance
 [LongestCommonSubsequence]: https://en.wikipedia.org/wiki/Longest_common_subsequence_problem
 [JaroWinkler]: https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
+[Jaccard]: https://en.wikipedia.org/wiki/Jaccard_index
 
 # Examples
 
---
+++ lib/src/jaccard.dart
@@ -0,0 +1,88 @@
+// Copyright (c) 2016, Istvan Soos. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import './base.dart';
+
+/// The Jaccard coefficient measures similarity between finite sample sets, and
+/// is defined as the size of the intersection divided by the size of the union
+/// of the sample sets.
+///
+/// The strings are divided into N-grams (N-character sequences), and the
+/// Jaccard distance is calculated based on these sets.
+/// https://en.wikipedia.org/wiki/Jaccard_index
+class Jaccard implements StringDistance, NormalizedStringDistance {
+  /// The size of the N-character sequences.
+  final int ngram;
+
+  /// Whether to introduce extra padding before and after the text.
+  final bool usePadding;
+
+  Jaccard({this.ngram: 2, this.usePadding: false});
+
+  @override
+  int distance(String s1, String s2) {
+    if (usePadding) {
+      s1 = _pad(s1);
+      s2 = _pad(s2);
+    }
+    Set<String> set1 = _split(s1);
+    Set<String> set2 = _split(s2);
+    int intersection = set1.where((item) => set2.contains(item)).length;
+    int union = set1.length + set2.length - intersection;
+    return union - intersection;
+  }
+
+  @override
+  double normalizedDistance(String s1, String s2) {
+    if (usePadding) {
+      s1 = _pad(s1);
+      s2 = _pad(s2);
+    }
+    Set<String> set1 = _split(s1);
+    Set<String> set2 = _split(s2);
+    int intersection = set1.where((item) => set2.contains(item)).length;
+    int union = set1.length + set2.length - intersection;
+    return (union - intersection) / union;
+  }
+
+  String _pad(String s) {
+    String pad = ''.padLeft(ngram - 1);
+    return '$pad${s.trim()}$pad';
+  }
+
+  Set<String> _split(String s) {
+    Set<String> set = new Set();
+    if (s.length <= ngram) {
+      set.add(s);
+    } else {
+      for (int i = 0; i <= s.length - ngram; i++) {
+        set.add(s.substring(i, i + ngram));
+      }
+    }
+    return set;
+  }
+}
+
+/// Combines multiple Jaccard normalized edit distance of N=1, N=2, N=3, ...
+/// The individual distances are weighed by N^2.
+class CombinedJaccard implements NormalizedStringDistance {
+  List<Jaccard> _list;
+  List<int> _weights;
+  int _sumWeights = 0;
+
+  CombinedJaccard({int ngram: 5, bool usePadding: false}) {
+    _list = new List.generate(
+        ngram, (i) => new Jaccard(ngram: i + 1, usePadding: usePadding));
+    _weights = new List.generate(ngram, (i) => (i + 1) * (i + 1));
+    _sumWeights = _weights.fold(0, (a, b) => a + b);
+  }
+
+  @override
+  double normalizedDistance(String s1, String s2) {
+    double distance = 0.0;
+    for (int i = 0; i < _list.length; i++) {
+      distance += _list[i].normalizedDistance(s1, s2) * _weights[i];
+    }
+    return distance / _sumWeights;
+  }
+}
